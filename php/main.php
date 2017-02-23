d9ea3b9702d57b0697f502f1e159fa26a82afd77
MouseTor <mouse-saenko@bk.ru>
2017-02-23 12:30:37 +0300

    New design first template

--- php/main.php
+++ php/main.php
@@ -60,34 +60,12 @@
                 <form>
                     <div class='form-block'>
                         <p id='hide-form-button' data-login-session='".$_SESSION['sessionlogin']."'>▼ </p>
-                        <p>ФИО</p>
-                        <input type='text' name='clientSurName' tabindex='1' maxlength = '20'>
-                        <input type='text' name='clientName' tabindex='2' maxlength ='20'>
-                        <input type='text' name='clientLastName' tabindex='3' maxlength = '20'>
-                        <p>Номер телефона</p>
-                        <b>+7</b><input type='text' name='clientPhoneNumber' tabindex='4' maxlength = 10>
-                        <p><input type='submit' value='Записать' name='insertApp' tabindex='9' id='submit-data-button' data-load-script='false'><input type='submit' value='Обновить' name='reload'></p>
-                        <p class='main-error-log'></p>
-                        <input type='hidden' name='oldClientId'>
-                        <div class='slideUpClientName'></div>
                     </div>
                     <div class='form-block'>
-                        <p><b>Добавление записи №</b></p>
-                        <p>Населенный пункт</p>
-                        <input type='text' name='clientCity' tabindex='5' maxlength = '20'>
-                        <p>Стоимость</p>
-                        <input type='text' name='appCost' tabindex='7' maxlength = '6'>
-                    </div>
-                    <div class='form-block'>
-                        <p class='new-app-numb'>1</p>
-                        <p>Адрес</p>
-                        <textarea name='clientAddress' cols='30' rows='4' tabindex='6' maxlength = '124'></textarea>
                         <p><input type='submit' value='Только невыполненные' name='appfilter-butt'></p>
                     </div>
                     <div class='form-block'>
-                        <p><a href='../about.html'>О программе</a></p>
-                        <p>Описание</p>
-                        <textarea name='appDescr' cols='27' rows='4' tabindex='8' maxlength = '124'></textarea> 
+                        <p><a href='../about.html'>О программе</a></p> 
                         <p><input type='submit' value='Выйти из системы' id='end-programm'></p>                   
                     </div>
                 </form>
@@ -146,6 +124,30 @@
             </div>
         </div>
     
+    </div>
+    <div class="new-app-panel-wraper">
+        <div class="new-app-panel">
+            <p>ФИО</p>
+            <input type='text' name='clientSurName' tabindex='1' maxlength = '20'>
+            <input type='text' name='clientName' tabindex='2' maxlength ='20'>
+            <input type='text' name='clientLastName' tabindex='3' maxlength = '20'>
+            <p>Номер телефона</p>
+            <b>+7</b><input type='text' name='clientPhoneNumber' tabindex='4' maxlength = 10>
+            <p><input type='submit' value='Записать' name='insertApp' tabindex='9' id='submit-data-button' data-load-script='false'><input type='submit' value='Обновить' name='reload'></p>
+            <p class='main-error-log'></p>
+            <input type='hidden' name='oldClientId'>
+            <div class='slideUpClientName'></div>
+            <p><b>Добавление записи №</b></p>
+            <p>Населенный пункт</p>
+            <input type='text' name='clientCity' tabindex='5' maxlength = '20'>
+            <p>Стоимость</p>
+            <input type='text' name='appCost' tabindex='7' maxlength = '6'>
+            <p class='new-app-numb'>1</p>
+            <p>Адрес</p>
+            <textarea name='clientAddress' cols='30' rows='4' tabindex='6' maxlength = '124'></textarea>
+            <p>Описание</p>
+            <textarea name='appDescr' cols='27' rows='4' tabindex='8' maxlength = '124'></textarea>
+        </div>
     </div>
 </body>
 </html>
